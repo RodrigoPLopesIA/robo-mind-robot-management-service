fd0e0e4aec274a72ded9facbe0521666f056a69a
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2026-07-24 00:14:08 -0300

    feat: create project

---
+++ src/main/java/com/robomind/robot_management_service/RobotManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.robomind.robot_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robomind/robot_management_service/RobotManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robomind.robot_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
